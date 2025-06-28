1181d6b25458e42470300602240a6fab1c3e47ef
Bvisi0n <jaro.de.graeve@outlook.com>
2025-06-28 15:20:11 +0200

    Tweak Matrix2D constructor formatting for visual alignment
    - Aligned matrix initializer list for readability
    - No code changes; purely cosmetic
    See #29

--- GameMathLib/src/Matrix2D.cpp
+++ GameMathLib/src/Matrix2D.cpp
@@ -4,9 +4,9 @@ namespace GameMath {
     // Default to identity: this is the most common 2D transform initializer
     // and avoids unnecessary overwrites in >80% of real-world use cases.
 	Matrix2D::Matrix2D(bool identity)
-        : matrix{{1.0f, 0.0f, 0.0f},
-            {0.0f, 1.0f, 0.0f},
-            {0.0f, 0.0f, 1.0f}}
+        : matrix{   {1.0f, 0.0f, 0.0f},
+                    {0.0f, 1.0f, 0.0f},
+                    {0.0f, 0.0f, 1.0f}}
     {
         if (!identity) {
             matrix[0][0] = matrix[1][1] = matrix[2][2] = 0.0f;
